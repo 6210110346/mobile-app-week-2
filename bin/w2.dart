2f43c8b78104bec4ffddcb9301579c608d70721e
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-05 20:55:17 +0700

    6

--- bin/w2.dart
+++ bin/w2.dart
@@ -1,14 +1,35 @@
-const String str = 'on 1 0 set inc inc inc set inc inc inc set set set inc';
+const String str = '''on 18 0
+set
+inc
+set
+inc
+set
+inc
+inc
+set''';
 
 void main(List<String> arguments) {
-  var commands = str.split(' ');
-  Clock clock;
-  for (var i = 0; i < commands.length; i++) {
-    if (commands[i] == 'on') {
-      clock = Clock(int.parse(commands[i + 1]), int.parse(commands[i + 2]));
-    }
-  }
+  var commands = str.split('\n');
+  var clock = Clock(0, 0, 0);
+  var clockOn = false;
+  // for (var i = 0; i < commands.length; i++) {
+  //   if (commands[i].contains('on ') && !clockOn) {
+  //     var currentState =
+  //         (commands[i + 1] == '0' && commands[i + 2] == '0') ? true : false;
+  //     clock = Clock(int.parse(commands[i + 1]), int.parse(commands[i + 2]),
+  //         currentState ? 1 : 0);
+  //     clockOn = true;
+  //   }
+  // }
   for (var command in commands) {
+    if (command.contains('on ')) {
+      var items = command.split(' ');
+      for(var item in items){
+        if(( value = int.tryParse(item)) != null){
+          var clockone = Clock(hour, minute, currentState)
+        }
+      }
+    }
     if (clock.currentState != null) {
       if (command == 'set') {
         clock.nextState();
@@ -17,10 +38,12 @@ void main(List<String> arguments) {
       }
     }
   }
+
+  print(clock.states);
 }
 
 class Clock {
-  Clock(int hour, int minute) {
+  Clock(int hour, int minute, this.currentState) {
     states = <State>[
       Idle(),
       SettingHour(time: hour),
@@ -34,11 +57,15 @@ class Clock {
   }
 
   void inc() {
+    if (states[currentState]?.time == 59) {
+      states[1].inc();
+    }
     states[currentState].inc();
   }
 }
 
 class Idle extends State {
+  var time = 0;
   @override
   void inc() {}
 }
