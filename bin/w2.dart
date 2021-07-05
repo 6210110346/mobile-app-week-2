d02c56179394e5a7009534e821175ac46a01b58e
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-05 19:27:41 +0700

    2

--- bin/w2.dart
+++ bin/w2.dart
@@ -1,3 +1,53 @@
+const String str = 'on 1 0 set inc inc inc set inc inc inc set set set inc';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  var commands = str.split(' ');
+  for (var command in commands) {
+    if (command == 'on') {
+      print('yes');
+    }
+  }
 }
+
+class Idle extends State {
+  final SettingHour hour;
+  final SettingMinute minute;
+  Idle(this.hour, this.minute);
+
+  @override
+  String toString() {
+    return '$hour : $minute';
+  }
+}
+
+class SettingHour extends State {
+  int time;
+
+  SettingHour({this.time = 0});
+
+  void inc() {
+    time = (time + 1) % 24;
+  }
+
+  @override
+  String toString() {
+    return time.toString();
+  }
+}
+
+class SettingMinute extends State {
+  int time;
+
+  SettingMinute({this.time = 0});
+
+  void inc() {
+    time = (time + 1) % 60;
+  }
+
+  @override
+  String toString() {
+    return time.toString();
+  }
+}
+
+abstract class State {}
